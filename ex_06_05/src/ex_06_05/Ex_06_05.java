fd9d34f0f87a5840cdd4611961c3420c380a9408
bluewind <bluewindice@gmail.com>
2014-12-23 22:10:42 +0900

    modified ex_06_05

--- ex_06_05/src/ex_06_05/Ex_06_05.java
+++ ex_06_05/src/ex_06_05/Ex_06_05.java
@@ -1,19 +1,59 @@
 package ex_06_05;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Ex_06_05 {
 
     public static void main(String[] args) {
-        ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
         
-        map.merge("aaa", 1, (existingValue, newValue) -> existingValue + newValue);
-        System.out.println(map.get("aaa"));
-        map.merge("aaa", 1, (existingValue, newValue) -> existingValue + newValue);
-        System.out.println(map.get("aaa"));
-        map.merge("aaa", 10, (existingValue, newValue) -> existingValue + newValue);
-        System.out.println(map.get("aaa"));
+    }
+    
+    public static Map<String, Set<File>> classifyWords(File[] files) throws InterruptedException{
+        if(files == null){
+            throw new NullPointerException("files is null.");
+        }
         
+        CountDownLatch latch = new CountDownLatch(files.length);
+        Map<String, Set<File>> map = new ConcurrentHashMap<>();
+        for(File file : files){
+            new Thread( ()->{
+                try {
+                    String contents = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+                    String[] words = contents.split("[\\P{L}]+");
+                   
+                    Set<File> fileSet = new HashSet<>();
+                    fileSet.add(file);
+                    for(String word : words){
+                        map.merge(word, fileSet, (existingSet, newSet) -> {
+                                                    Set<File> mergedSet = new HashSet<>(); 
+                                                    mergedSet.addAll(existingSet); 
+                                                    mergedSet.addAll(newSet); 
+                                                    return mergedSet;}
+                        );
+                    }
+                    
+                    latch.countDown();
+                } catch (IOException ex) {
+                    Logger.getLogger(Ex_06_05.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }).start();
+        }
+        
+        latch.await();
+        return map;
     }
     
 }
---
+++ ex_06_05/test/test_06_05/Test_06_05.java
@@ -0,0 +1,44 @@
+
+package test_06_05;
+
+import static ex_06_05.Ex_06_05.*;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class Test_06_05 {
+
+    @Test
+    public void test() throws InterruptedException{
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        File file3 = new File("file3.txt");
+        
+                
+        File[] files = new File[]{file1, file2, file3};
+        Map<String, Set<File>> map = classifyWords(files);
+        
+        Set<File> kaSet = map.get("ka");
+        Set<File> expectedSet = new HashSet<>();
+        expectedSet.add(file1);
+        expectedSet.add(file2);
+        expectedSet.add(file3);
+        assertEquals(kaSet, expectedSet);
+        
+        Set<File> aSet = map.get("a");
+        expectedSet.clear();
+        expectedSet.add(file2);
+        expectedSet.add(file3);
+        assertEquals(aSet, expectedSet); 
+        
+        Set<File> ASet = map.get("A");
+        expectedSet.clear();
+        expectedSet.add(file3);
+        assertEquals(ASet, expectedSet);         
+    }
+    
+}
