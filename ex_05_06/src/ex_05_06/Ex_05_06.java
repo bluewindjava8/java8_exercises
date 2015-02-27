a2d70f297d9cc501981c9d5942dce1ad26083438
bluewind <bluewindice@gmail.com>
2015-02-27 11:50:40 +0900

    Added from ex_05_06

--- ex_05_06/src/ex_05_06/Ex_05_06.java
+++ ex_05_06/src/ex_05_06/Ex_05_06.java
@@ -10,11 +10,13 @@ public class Ex_05_06 {
 
 
     public static void main(String[] args) {
-        LocalDate start = LocalDate.of(1900, 1, 1);
-        LocalDate end = LocalDate.of(1999, 12, 31);
+        LocalDate start = LocalDate.of(1901, 1, 1);
+        LocalDate end = LocalDate.of(2000, 12, 31);
         start = start.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
         for(LocalDate date = start; !date.isAfter(end); date = date.plusDays(7)){
-            System.out.println(date);
+            if(date.getDayOfMonth() == 13){
+                System.out.println(date);
+            }
         }
         
     }
